aefa59f9695ebb1e6fc93f4417cc1d5d1b026798
kevinalvarel <alvarelkevin12@gmail.com>
2024-04-04 20:17:38 +0700

    update

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const form = document.querySelector("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
 
 function sendEmail() {
+  const bodyMessage = "Name: ${name.value}";
+
   Email.send({
     Host: "smtp.elasticemail.com",
     Username: "muhammadkevinalvarel@gmail.com",
-    Password: "4648A982511191D3921B71C37BE31EF58FB7",
-    To: "them@muhammadkevinalvarel.com",
-    From: "you@muhammadkevinalvarel.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
+    Password: "29D7B263E07E03DF5FA919DEC6045927C682",
+    To: "muhammadkevinalvarel@gmail.com",
+    From: "muhammadkevinalvarel@gmail.com",
+    Subject: "Ini Subject",
+    Body: bodyMessage,
   }).then((message) => alert(message));
 }
 
